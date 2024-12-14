e8de2c5b92e380d0b21fef217d64bc95b9ca9de6
MaheshBastawade <maheshab051@gmail.com>
2024-12-14 12:08:33 +0530

    code_upload

---
+++ src/practice_conflicts/PracticeConflict1.java
@@ -0,0 +1,9 @@
+package practice_conflicts;
+
+public class PracticeConflict1 {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello_class1");
+	}
+
+}
---
+++ src/practice_conflicts/PracticeConflict2.java
@@ -0,0 +1,9 @@
+package practice_conflicts;
+
+public class PracticeConflict2 {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello_class1");
+	}
+
+}
