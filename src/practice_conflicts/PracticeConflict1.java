5fd6473c72250f491225d71506ecf91456b1e5b2
MaheshBastawade <maheshab051@gmail.com>
2024-12-14 12:19:04 +0530

    Branch B1 changes

--- src/practice_conflicts/PracticeConflict1.java
+++ src/practice_conflicts/PracticeConflict1.java
@@ -3,7 +3,7 @@ package practice_conflicts;
 public class PracticeConflict1 {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello_class1");
+		System.out.println("Hello_class1 Changes in branch B1");
 	}
 
 }
