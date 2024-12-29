52cc8820a0a1924244c1c6fe3f2a5a97bb40faf2
MaheshBastawade <maheshab051@gmail.com>
2024-12-29 12:11:01 +0530

    commmit_1

--- src/practice/Practice_03.java
+++ src/practice/Practice_03.java
@@ -5,5 +5,6 @@ public class Practice_03 {
 	public static void main(String[] args) {
 		System.out.println("Hello_class1");
 		System.out.println("Branch B changes");
+		System.out.println("commit_1");
 	}
 }
